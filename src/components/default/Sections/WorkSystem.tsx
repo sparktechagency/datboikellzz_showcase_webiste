4cdc558f50ea938b58081029131304d1d05f52ff
hosainahmed <hosainahmed534745@gmail.com>
2025-06-15 18:31:34 +0600

    try to make more dynamic

--- src/components/default/Sections/WorkSystem.tsx
+++ src/components/default/Sections/WorkSystem.tsx
@@ -12,6 +12,12 @@ function WorkSystem() {
             icon: <BookIcon />
         },
         {
+            title: '',
+            description: '',
+            icon: '',
+            invisble: true
+        },
+        {
             title: 'Pick a Plan',
             description: 'Choose the subscription that fits your betting style.',
             icon: <PlaneSvg />
@@ -27,18 +33,18 @@ function WorkSystem() {
             <div>
                 <h1 className="font-poppins font-semibold text-5xl text-center tracking-normal leading-none">How BetWise Works</h1>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-7 px-3">
+            <div className="grid grid-cols-1 md:grid-cols-3 px-3">
                 {
                     workFlow.map((item, index) => (
                         <div key={index} className='relative p-6 md:p-12 rounded-md overflow-hidden shadow-sm md:shadow-2xl flex flex-col md:items-start'>
-                            <div className='absolute -z-1 top-0 left-0 w-full h-full'>
+                            {!item.invisble && <div className='absolute -z-1 top-0 left-0 w-full h-full'>
                                 <Image
                                     src="/work-bg.png"
                                     alt="work-system-bg"
                                     fill
                                     className='object-bottom-right'
                                 />
-                            </div>
+                            </div>}
                             <div className='w-20 h-20  flex items-start justify-center rounded-full'>
                                 {item.icon}
                             </div>
