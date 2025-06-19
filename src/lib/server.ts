1be779f950bf0ca904d03a72325a7b4b1b7ccb47
hosain ali <hosainahmed534745@gmail.com>
2025-06-19 15:39:07 +0600

    update ip in the scripts

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,5 +1,5 @@
-export const url = 'http://18.158.237.149:8002';
-
+export const url = 'http://18.158.237.149:8001';
+ 
 export const imageUrl = (image: string) => {
     return image
         ? image?.startsWith('http')
