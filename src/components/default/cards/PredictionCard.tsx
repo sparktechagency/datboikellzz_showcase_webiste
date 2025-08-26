fa4de3d145fd8fad83864a7e17bbfad123b6de1e
hosainahmeed <skyfal430@gmail.com>
2025-08-26 18:18:10 +0600

    update title bar

--- src/components/default/cards/PredictionCard.tsx
+++ src/components/default/cards/PredictionCard.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Image from 'next/image';
 import { imageUrl } from '@/lib/server';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function PredictionCard({ post }: any) {
     return (
-        <div className="w-full border border-gray-100 bg-gradient-to-tr to-green-300/40 via-white from-white bg-white  rounded-xl  p-6">
+        <div className="w-full border border-gray-100 bg-gradient-to-tr to-green-300/40 via-white from-green-300/20 bg-white  rounded-xl  p-6">
             <div className="w-full flex items-center justify-between mb-4">
                 <div className="flex items-center  gap-2">
                     <Image
@@ -44,6 +45,23 @@ function PredictionCard({ post }: any) {
                     className="w-8 h-auto"
                 />
                 <h1>{post?.predictionDescription}</h1>
+            </div>
+            <div className='flex  items-center mt-6 justify-between gap-2'>
+                <div className='flex gap-2'>
+                    <Image
+                        width={50}
+                        height={50}
+                        src={'/stats.svg'}
+                        alt=""
+                        className="w-6 h-auto"
+                    />
+                    <h1 className='text-black font-bold'>Stats & Badges</h1>
+                </div>
+                <div className='flex items-start flex-col gap-2'>
+                    <div className="py-1 px-4 text-sm bg-[#FEF3C7] border border-[#F59E0B]  text-[#F59E0B] rounded-md">
+                        {`${post?.targetUser} Analyses`}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/shared/TitleBar.tsx
+++ src/components/shared/TitleBar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function TitleBar({ title }: Readonly<{ title: string }>) {
     return (
-        <div>
-            <h1 className=''>{title}</h1>
+        <div className='bg-gradient-to-bl from-[#F8FAFC] via-[#F8FAFC] to-green-300/40 mb-4 p-2'>
+            <h1 className='text-3xl font-bold'>{title}</h1>
         </div>
     )
 }
